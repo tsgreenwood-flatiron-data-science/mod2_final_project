d3043981d43bd294c06e16b43663c189e33f4e8f
TSGreenwood <greenwood.ts@gmail.com>
2019-05-10 17:05:07 -0700

    Walk through final notebook and clean files

--- visualize_multiple_series.py
+++
@@ -1,7 +0,0 @@
-def get_top_author_dfs(data, top_authors):
-    """Return list of dataframes of top authors."""
-    dfs = []
-    for author in top_authors:
-        df = data[data['author'] == author]
-        dfs.append(df)
-    return dfs
